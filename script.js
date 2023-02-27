47790f62a2a3c8c39eb1e4aac8b8fd589de58242
maghradz3 <120563893+maghradz3@users.noreply.github.com>
2023-02-27 10:09:23 +0400

    data tabs and menu fade animation

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector(`#section--1`);
+
+const nav = document.querySelector('.nav');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -33,10 +38,9 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//Implementing Smooth Scroling =old method
+////////////////////////////////////////
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector(`#section--1`);
+//Implementing Smooth Scroling =old method
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
@@ -50,6 +54,7 @@ btnScrollTo.addEventListener('click', function (e) {
     document.documentElement.clientWidth
   );
 
+  section1.scrollIntoView({ behavior: 'smooth' });
   //Scroling
   // window.scrollTo(
   //   s1coords.left + window.pageXOffset,
@@ -63,5 +68,79 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
 
   //Modern Way!!!!
-  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+
+//1.Add event listeren to common parent element
+//2..determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    console.log(`link`);
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  //guard Close = დამცავი დახურვა
+  if (!clicked) return;
+  //Active tab
+
+  //remove clases
+  tabs.forEach(el => el.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  //actovate class
+  clicked.classList.add('operations__tab--active');
+
+  //Active contact area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+///////////////3- Menu fade animation//////////////////////////////
+const handlerHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link);
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    console.log(siblings);
+    const logo = link.closest('nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//Passing argument into hendler
+nav.addEventListener('mouseover', handlerHover.bind(0.5));
+
+nav.addEventListener('mouseout', handlerHover.bind(1));
+
+////////////////////Sticy nav-Bar//////////////////////////
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
 });
